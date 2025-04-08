757855ca7a969bc421f9ab039ab4595c916b66b0
Matt Tsogt <mandah0628@gmail.com>
2025-04-08 16:51:45 -0600

    fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_EXPRESS_BASE_URL || 'http://localhost:5050',
+  baseURL: process.env.NEXT_PUBLIC_EXPRESS_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
